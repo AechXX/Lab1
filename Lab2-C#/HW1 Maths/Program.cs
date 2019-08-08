215b81842fd7f53e33b2db86f01e9ea8ce67064a
AechXX <hkdaven@gmail.com>
2019-08-08 10:50:23 -0500

    HW 1 done

---
+++ Lab2-C#/HW1 Maths/Program.cs
@@ -0,0 +1,44 @@
+﻿/*
+Project:    HW1 - Variables and Arithmetic
+Date:       08/08/2019
+Author:     Halley Davenport
+Notes:      This program reads a character's stats
+            and determines their attack bonus.
+*/
+
+using System;
+
+namespace Lab_2_Calculate_Batting_Average
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //variable declaration
+            string strCharacterName;
+            int nAttackBonus, nStrOrDex;
+            int nToHitTotal;
+
+            //prompt for name and receive
+            Console.WriteLine("~This program calculates a character's total attack bonus.~");
+            Console.WriteLine("<------------------------------------------------------------>");
+            Console.WriteLine("");
+            Console.WriteLine("Enter the character's name: ");
+            strCharacterName = Console.ReadLine();
+
+            //prompt for stats and attack bonus
+            Console.WriteLine("Enter the character's base attack score: ");
+            nAttackBonus = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the character's Strength (melee) or Dexterity (ranged) bonus: ");
+            nStrOrDex = Convert.ToInt32(Console.ReadLine());
+
+            //calculation
+            nToHitTotal = nAttackBonus + nStrOrDex;
+
+            Console.WriteLine(strCharacterName + "'s total to hit bonus is " + nToHitTotal);
+            Console.ReadLine();
+
+        }
+    }
+}
