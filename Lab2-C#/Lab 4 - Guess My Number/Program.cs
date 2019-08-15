053313842bb4e12f5184dd1a058062fc98e30fb1
AechXX <hkdaven@gmail.com>
2019-08-15 10:23:09 -0500

    Lab 4 Guess Number done

---
+++ Lab2-C#/Lab 4 - Guess My Number/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Lab_4___Guess_My_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            int hidden;
+            int nGuess;
+
+            while (true)
+            {
+                hidden = rand.Next(100);
+                Console.WriteLine(hidden);
+                
+                while (true)
+                {                     
+                    Console.WriteLine("Guess a number between 0 and 100: ");
+                while (!int.TryParse(Console.ReadLine(), out nGuess))
+                {
+                    Console.WriteLine("Guess a number between 0 and 100: ");
+                }
+
+                if (nGuess == hidden)
+                {
+                    Console.WriteLine("Ding, ding, ding! We have a winner!");
+                    Console.WriteLine("");
+                        break;
+                }
+
+                else if (nGuess < hidden)
+                {
+                    Console.WriteLine("Too low. Try again.");
+                    Console.WriteLine("");
+                    }
+
+                else if (nGuess > hidden)
+                {
+                    Console.WriteLine("Too high. Guess again.");
+                    Console.WriteLine("");
+                    }
+                }
+            }
+
+        }
+    }
+}
